c33adfaafe120c14a732ff5458d2cb8acf03752f
Merame <egoistgray@gmail.com>
2023-04-14 14:09:03 +0800

    feat(shell): using ps7 powershell

--- lua/akumillis/windows.lua
+++ lua/akumillis/windows.lua
@@ -1,1 +1,6 @@
-vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+vim.opt.clipboard:prepend{'unnamed', 'unnamedplus'}
+
+vim.opt.shell = "pwsh"
+vim.opt.shellcmdflag = "-command"
+vim.opt.shellquote = "\\\""
+vim.opt.shellxquote = ""
